a418c388967181556184afd8af16deae4a7883a0
Mephisto Lin <Mephisto007linlin@outlook.com>
2025-06-08 23:20:21 +0800

    feat: [全域搜尋]  線性搜尋法，找尋特定元素的位置，第一版

---
+++ ConsoleConsoleGlobalSearch2/ConsoleConsoleGlobalSearch2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+/*
+ *  取得特定元素存在的 "注標"
+*/
+namespace ConsoleConsoleGlobalSearch2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, v;
+
+            // 提示並讀入陣列長度
+            Console.Write("請輸入陣列長度 : ");
+            N = int.Parse(Console.ReadLine());
+
+            // 建立整數陣列
+            int[] vec = new int[N];
+
+            // 提示並讀入目標值
+            Console.Write("請輸入要尋找的元素 : ");
+            v = int.Parse(Console.ReadLine());
+
+            // 提示並逐一讀入陣列元素
+            Console.WriteLine("請依序輸入陣列元素 : ");
+            for (int i = 0; i < N; i++)
+            {
+                vec[i] = int.Parse(Console.ReadLine());
+            }
+
+            // 線性搜尋
+            int found_id = -1;  // 初始值設為 -1 等不可能的值
+            for (int i = 0; i < N; i++)
+            {
+                if (vec[i] == v)
+                {
+                    found_id = i;  // 找到的話就紀錄注標
+                    break;
+                }
+            }
+
+            if (found_id == -1)
+                Console.WriteLine("沒有找到。");
+            else
+                Console.WriteLine($"位置 : {found_id}");
+        }
+    }
+}
